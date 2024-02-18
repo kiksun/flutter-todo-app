ba442aa1a2f939990bcd7ec6a25b34f500bd6482
kiksun <kimera4173@gmail.com>
2024-02-18 22:47:07 +0900

    add filtering button

--- lib/screen/todo_screen.dart
+++ lib/screen/todo_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo/enum.dart';
 import 'package:todo/model/todo.dart';
+import 'package:todo/provider/filter_provider.dart';
 import 'package:todo/provider/todo_list_provider.dart';
 import 'package:todo/screen/todo_item.dart';
+
+import '../model/filter.dart';
 
 Todo a() => const Todo(
     id: 4, content: 'hogehoge', completed: false, priority: Priority.mid);
@@ -14,15 +17,18 @@ class TodoScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final todoList = ref.watch(todoListProvider);
+    final todoFilter = ref.watch(todoFilterProvider);
+    final filteredTodoList = getFilterdTodoList(todoList, todoFilter);
+    print(todoFilter);
     return Column(
       children: [
         ListView.builder(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
-          itemCount: todoList.length,
+          itemCount: filteredTodoList.length,
           itemBuilder: (BuildContext context, int index) {
             return TodoItem(
-              todoContent: todoList[index],
+              todoContent: filteredTodoList[index],
               onChange: (value) {
                 ref.read(todoListProvider.notifier).toggle(index);
               },
@@ -35,7 +41,58 @@ class TodoScreen extends ConsumerWidget {
             child: Text('add Todo'),
           ),
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    todoFilter.filterLow ? Colors.grey : Colors.blue,
+              ),
+              onPressed: () => ref
+                  .read(todoFilterProvider.notifier)
+                  .priorityChange(Priority.low),
+              child: const Text('Low'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    todoFilter.filterMid ? Colors.grey : Colors.green,
+              ),
+              onPressed: () => ref
+                  .read(todoFilterProvider.notifier)
+                  .priorityChange(Priority.mid),
+              child: const Text('Mid'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    todoFilter.filterHigh ? Colors.grey : Colors.red,
+              ),
+              onPressed: () => ref
+                  .read(todoFilterProvider.notifier)
+                  .priorityChange(Priority.high),
+              child: const Text('High'),
+            ),
+          ],
+        )
       ],
     );
   }
+}
+
+List<Todo> getFilterdTodoList(List<Todo> todoList, Filter filter) {
+  List<Priority> a = [Priority.low, Priority.mid, Priority.high];
+  print('AAAAA $a');
+  if (filter.filterLow) {
+    a.remove(Priority.low);
+  }
+  if (filter.filterMid) {
+    a.remove(Priority.mid);
+  }
+  if (filter.filterHigh) {
+    a.remove(Priority.high);
+  }
+  print('AAAAA $a');
+  return todoList.where((element) => a.contains(element.priority)).toList();
 }
